d8270bd24b5c8e4b164723ccd0af611e433439a3
Sye-5pace <owusilasyp00@gmail.com>
2024-06-21 08:43:36 +0100

    initial commit create task #1

---
+++ main.js
@@ -0,0 +1,8 @@
+//Task #1 Define a superhero object
+let superhero = {
+  name: 'Superman',
+  secretIdentity: 'Clark Kent',
+  powers: ['flight', 'super strength', 'heat vision'],
+  weakness: 'Kryptonite'
+};
+
